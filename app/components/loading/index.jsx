588c7753b235f0a54b3f1602bb95943b50b375a2
Will Schwanke <wschwanke@gmail.com>
2018-07-31 18:06:00 -0500

    Fix JSX errors in Loading component.

--- app/components/loading/index.jsx
+++ app/components/loading/index.jsx
@@ -1,32 +1,17 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-const Loading = text => (
-  <div class="Loading">
-    <div class="Loading__cube Loading__cube--1"></div>
-    <div class="Loading__cube Loading__cube--2"></div>
-    <div class="Loading__cube Loading__cube--3"></div>
-    <div class="Loading__cube Loading__cube--4"></div>
-    <div class="Loading__cube Loading__cube--5"></div>
-    <div class="Loading__cube Loading__cube--6"></div>
-    <div class="Loading__cube Loading__cube--7"></div>
-    <div class="Loading__cube Loading__cube--8"></div>
-    <div class="Loading__cube Loading__cube--9"></div>
+const Loading = () => (
+  <div className="Loading">
+    <div className="Loading__cube Loading__cube--1" />
+    <div className="Loading__cube Loading__cube--2" />
+    <div className="Loading__cube Loading__cube--3" />
+    <div className="Loading__cube Loading__cube--4" />
+    <div className="Loading__cube Loading__cube--5" />
+    <div className="Loading__cube Loading__cube--6" />
+    <div className="Loading__cube Loading__cube--7" />
+    <div className="Loading__cube Loading__cube--8" />
+    <div className="Loading__cube Loading__cube--9" />
   </div>
 );
-
-/**
- * Prop types
- */
-Loading.propTypes = {
-  text: PropTypes.string,
-};
-
-/**
- * Default props
- */
-Loading.defaultProps = {
-  text: 'Loading...',
-};
 
 export default Loading;
